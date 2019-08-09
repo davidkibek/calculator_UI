4c067a424f5935b4d98a4644b1f4caa14a05eb6a
david kibe <kibek02@gmail.com>
2019-08-09 18:39:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const row = document.getElementsByClassName('rowPad');
+const input = document.getElementsByClassName('inputCss');
+const result = document.getElementsByClassName('resultCss');
+const inputdata = document.getElementById('inputData');
+const result = document.getElementById('result');
